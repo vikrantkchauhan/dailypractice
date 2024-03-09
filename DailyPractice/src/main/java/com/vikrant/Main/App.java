56c5498820599f2656f03691ca8e4c5707b422ff
Vikrant Chauhan <vchauhan2025@gmail.com>
2024-03-09 19:22:26 +0530

    Initial commit of project setup

---
+++ DailyPractice/src/main/java/com/vikrant/Main/App.java
@@ -0,0 +1,13 @@
+package com.vikrant.Main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
